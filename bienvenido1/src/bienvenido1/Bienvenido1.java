282fe391a17b7ab36344b2696d908fdf8bd2fd80
ferchuqui <luisfernandofarji@hotmail.com>
2017-06-25 13:36:57 -0300

    Primer programa en Java Imprimir una linea de texto

---
+++ bienvenido1/src/bienvenido1/Bienvenido1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bienvenido1;
+
+/**
+ *
+ * @author ferchuqui
+ * Extraido del libro Como programar en Java Pag 35
+ * El objetivo es imprimir una linea de texto en pantalla
+ * 
+ */
+public class Bienvenido1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println( "Bienvenido a la programacion en Java!" );
+        
+    }
+    
+}
