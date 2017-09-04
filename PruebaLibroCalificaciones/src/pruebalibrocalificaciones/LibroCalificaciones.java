3ebab74acf80983d70496ed5e706ff4440270cea
ferchuqui <luisfernandofarji@hotmail.com>
2017-09-04 01:11:44 -0300

    Creacion de libro de calificaciones

---
+++ PruebaLibroCalificaciones/src/pruebalibrocalificaciones/LibroCalificaciones.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebalibrocalificaciones;
+
+/**
+ *
+ * @author ferchuqui
+ */
+public class LibroCalificaciones {
+    //Muestra un mensaje de Bienvenida al usuario
+    public void mostrarMensaje()
+    {
+    System.out.println ( "Bienvenida al Libro de Calificaciones!");
+    }
+}
---
+++ PruebaLibroCalificaciones/src/pruebalibrocalificaciones/PruebaLibroCalificaciones.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebalibrocalificaciones;
+
+/**
+ *
+ * @author ferchuqui
+ */
+public class PruebaLibroCalificaciones {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Crea un objeto LibroCalificaciones y lo asigna a miLibroCalificaciones
+        LibroCalificaciones miLibroCalificaciones = new LibroCalificaciones();
+        
+        //llama al metodo mostrarMensaje de miLibroCalificaciones
+        miLibroCalificaciones.mostrarMensaje();
+    }
+    
+}
